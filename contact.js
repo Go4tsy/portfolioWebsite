c1a1888870b41aa4f8e21a86e420b6efad2d3816
Sean Etienne <mosesetienne@hotmail.com>
2022-06-28 09:36:37 -0400

    Readme Update

--- contact.js
+++ contact.js
@@ -2,12 +2,13 @@
 // // contact js 
 // const myTimeout = setTimeout(backmusic,3000)
 
-// function backmusic () {
-//     alert("Warning! background music check your volume! Enjoy :)")
-// }
 
 // document.querySelector("#Stop").addEventListener("click",myStopFunction)
 
 // // function myStopFunction() {
-// //     clearTimeout(backmusic)
-// // }
+    // //     clearTimeout(backmusic)
+    // // }
+    
+    // function backmusic () {
+    //     alert("Warning! background music check your volume! Enjoy :)")
+    // }
